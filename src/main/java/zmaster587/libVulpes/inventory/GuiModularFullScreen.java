3c71270e510fee9be9b4a3d41933c643a439f528
zerozaki666 <zerozaki666@outlook.com>
2026-07-29 23:22:09 +0800

    Revert "fix: keep fullscreen GUI coordinates scaled"
    This reverts commit 1006b6de832f27bcb39976996ec163c3d4988053.

--- src/main/java/zmaster587/libVulpes/inventory/GuiModularFullScreen.java
+++ src/main/java/zmaster587/libVulpes/inventory/GuiModularFullScreen.java
@@ -4,24 +4,29 @@ import java.util.List;
 
 import zmaster587.libVulpes.inventory.modules.IModularInventory;
 import zmaster587.libVulpes.inventory.modules.ModuleBase;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 
 public class GuiModularFullScreen extends GuiModular {
 
 	public GuiModularFullScreen(EntityPlayer playerInv,
-				List<ModuleBase> modules, IModularInventory modularInv,
-				boolean includePlayerInv, boolean includeHotBar, String name) {
+			List<ModuleBase> modules, IModularInventory modularInv,
+			boolean includePlayerInv, boolean includeHotBar, String name) {
 		super(playerInv, modules, modularInv, includePlayerInv,includeHotBar, name);
+		
+		this.xSize = Minecraft.getMinecraft().displayWidth;
+		this.ySize = Minecraft.getMinecraft().displayHeight;
+		this.width = Minecraft.getMinecraft().displayWidth;
+		this.height = Minecraft.getMinecraft().displayHeight;
 	}
-
+	
 	@Override
 	public void initGui() {
-		// GuiScreen.width/height are already expressed in scaled GUI units.
-		// Replacing them with framebuffer pixels breaks buttons and scissoring
-		// whenever Minecraft's GUI scale is not exactly two.
-		this.xSize = this.width;
-		this.ySize = this.height;
-
+		this.xSize = Minecraft.getMinecraft().displayWidth;
+		this.ySize = Minecraft.getMinecraft().displayHeight;
+		this.width = Minecraft.getMinecraft().displayWidth;
+		this.height = Minecraft.getMinecraft().displayHeight;
+		
 		super.initGui();
 	}
 }
